869195d69e710127dfd75eb7da559508b5464f10
Caltabiano, Joseph P <jcaltabiano@draper.com>
2019-07-02 16:19:06 -0400

    minimal

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Emap from "./maps";
+
+ReactDOM.render(<Emap />, document.getElementById("root"));
---
+++ src/maps.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Emap extends React.Component {
+  
+  render() {
+    
+    return (<div>Hello world!</div>)
+
+  }
+}
+
+export default Emap
