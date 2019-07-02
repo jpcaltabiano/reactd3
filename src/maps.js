6c21ccb61062c18204c1494e083bc19af8fd965a
Caltabiano, Joseph P <jcaltabiano@draper.com>
2019-07-02 16:24:52 -0400

    added data

--- src/maps.js
+++ src/maps.js
@@ -1,12 +1,14 @@
+//https://medium.com/@Elijah_Meeks/interactive-applications-with-react-d3-f76f7b3ebc71
+
 import React from "react";
 
 class Emap extends React.Component {
   
   render() {
     
-    return (<div>Hello world!</div>)
+    
 
   }
 }
 
-export default Emap
+export default (Emap)
